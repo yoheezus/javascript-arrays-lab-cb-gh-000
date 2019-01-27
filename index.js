9f1b17d90dbecd82ec95fe69fb81fa1d9ac76502
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 20:49:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ function appendKitten(name) {
     new_array = [...kittens, name]
     return new_array
 }
+
+function prependKitten(name) {
+    new_array = [name, ...kittens]
+    return new_array
+}
+
+function removeLastKitten() {
+    new_array = kittens.slice(0, kittens.length - 1)
+    return new_array
+}
+
+function removeFirstKitten() {
+    new_array = kittens.slice(1)
+    return new_array
+}
